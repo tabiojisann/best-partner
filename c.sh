e70fc957e13d2170e32fc802071453f121728adf
tabiojisann <takumiezumi@icloud.com>
2020-08-17 21:06:36 +0900

    fix

--- c.sh
+++ c.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 
-echo "実行するシェルを選択してください\nc : ciの構文エラーチェック\nd : コンテナに入る\ng : gitの変更をコミット、プッシュ"
+echo "実行するシェルを選択してください\nc : ciの構文エラーチェック\nd : コンテナに入る\ne : EC2インスタンスに接続\ng : gitの変更をコミット、プッシュ"
 
 read name
 
 case "$name" in
+    "c") sh ./scripts/ci_validate.sh
+    ;;
     "d") sh ./scripts/docker.sh
     ;;
-    "c") sh ./scripts/ci_validate.sh
+    "e") sh ./scripts/ec2.sh
     ;;
     "g") sh ./scripts/git.sh
     ;;
 esac
+
---
+++ scripts/ec2.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ssh -i ~/.ssh/yuruoji-key.pem ec2-user@54.248.158.153
