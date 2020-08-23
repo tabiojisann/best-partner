17eb30f8f2a0930393de3710f2d961c4284c01cf
tabiojisann <takumiezumi@icloud.com>
2020-08-23 09:39:59 +0900

    test

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -20,3 +20,5 @@ trait CreatesApplication
         return $app;
     }
 }
+
+
--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,4 +19,23 @@ class ArticleControllerTest extends TestCase
         ->assertViewIs('articles.index');
   }
 
+  public function testGuestCreate()
+  {
+    $response = $this->get(route('articles.create'));
+
+    $response->assertRedirect(route('login'));
+  }
+
+  public function testAuthCreate()
+  {
+    $user = factory(User::class)->create();
+
+    $response = $this->actingAs($user)
+                ->get(route('articles.create'));
+    
+    $response->assertStatus(200)
+             ->assertViewIs('articles.create');
+  }
+
 }
+
